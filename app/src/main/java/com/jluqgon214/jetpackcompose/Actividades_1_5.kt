d2985c73b1d9d3f467d799593341316d50ad691e
José Manuel Luque González <jluqgon214@g.educaand.es>
2023-11-10 18:05:44 +0100

    init

---
+++ app/src/main/java/com/jluqgon214/jetpackcompose/Actividades_1_5.kt
@@ -0,0 +1,144 @@
+package com.jluqgon214.jetpackcompose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+/*
+Actividad 1:
+Hacer que el texto del botón muestre "Cargar perfil", pero cambie a "Cancelar"
+cuando se muestre la línea de progreso... Cuando pulsemos "Cancelar" vuelve al texto por defecto.
+*/
+@Preview(showBackground = true)
+@Composable
+fun Actividad1() {
+    var showLoading by rememberSaveable { mutableStateOf(false) }
+    var changeText by rememberSaveable { mutableStateOf(false) }
+
+    Column(
+        Modifier
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        if (showLoading) {
+            CircularProgressIndicator(
+                color = Color.Red,
+                strokeWidth = 10.dp
+            )
+            LinearProgressIndicator(
+                modifier = Modifier.padding(top = 32.dp),
+                color = Color.Red,
+                trackColor = Color.LightGray
+            )
+        }
+
+        Button(
+            onClick = {
+                showLoading = !showLoading
+                changeText = !changeText
+            }
+        ) {
+            if (changeText){
+                Text(text = "Cargar perfil")
+            }
+            else Text(text = "Cancelar")
+
+        }
+    }
+}
+
+/*
+Actividad 2:
+Modifica ahora también que se separe el botón de la línea de progreso 30 dp,
+pero usando un estado... es decir, cuando no sea visible no quiero que tenga la separación
+aunque no se vea.
+*/
+
+
+/*
+Actividad 3:
+- Separar los botones entre ellos 10 dp, del indicador de progreso 15 dp y centrarlos horizontalmente.
+- Cuando se clique el botón Incrementar, debe añadir 0.1 a la propiedad progress y quitar 0.1
+  cuando se pulse el botón Decrementar.
+- Evitar que nos pasemos de los márgenes de su propiedad progressStatus (0-1)
+*/
+@Composable
+fun Actividad3() {
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LinearProgressIndicator(progress = 0f)
+
+        Row(Modifier.fillMaxWidth()) {
+            Button(onClick = { /* TODO */ }) {
+                Text(text = "Incrementar")
+            }
+            Button(onClick = { /* TODO */ }) {
+                Text(text = "Reducir")
+            }
+        }
+    }
+}
+
+
+/*
+Actividad 4:
+Sitúa el TextField en el centro de la pantalla y haz que reemplace el valor de una coma por un punto
+y que no deje escribir más de un punto decimal...
+*/
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Actividad4() {
+    var myVal by rememberSaveable { mutableStateOf("") }
+
+    TextField(
+        value = myVal,
+        onValueChange = { myVal = it },
+        label = { Text(text = "Importe") }
+    )
+}
+
+
+/*
+Actividad 5:
+Haz lo mismo, pero elevando el estado a una función superior y usando un componente OutlinedTextField
+al que debes añadir un padding alrededor de 15 dp y establecer colores diferentes en los bordes
+cuando tenga el foco y no lo tenga.
+A nivel funcional no permitas que se introduzcan caracteres que invaliden un número decimal.
+*/
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Actividad5() {
+    var myVal by rememberSaveable { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = myVal,
+        onValueChange = { myVal = it },
+        label = { Text(text = "Importe") }
+    )
+}
---
+++ app/src/main/java/com/jluqgon214/jetpackcompose/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.jluqgon214.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jluqgon214.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Actividad1()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewmyStateExample() {
+    var clicks by rememberSaveable { mutableStateOf(0) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { clicks++ }) {
+            Text(text = "Pulsar")
+        }
+
+        Text(text = "Has pulsado el boton $clicks veces")
+    }
+}
+
